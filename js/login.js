a0fd7eabb55f0664f8adc3c5d7027acb6c1e07c9
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-03-28 01:25:03 +0200

    Added user login functionality with backend integration by creating login.js

---
+++ js/login.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", function () {
+  //search for the login form
+  const loginForm = document.getElementById("loginForm");
+
+  // login
+  const loginLink = document.getElementById("loginLink");
+  loginLink.addEventListener("click", function (event) {
+    event.preventDefault();
+    loginForm.style.display = "block";
+  });
+
+  // add eventlistener for "submit" form
+  loginForm.addEventListener("submit", function (event) {
+    //
+    event.preventDefault();
+
+    // get the links of the inputs
+    const usernameInput = document.getElementById("loginUsername");
+    const passwordInput = document.getElementById("loginPassword");
+
+    // get the values and delete spaces
+    const username = usernameInput.value.trim();
+    const password = passwordInput.value.trim();
+
+    //check if username is empty
+    if (username === "") {
+      console.log("Username field can not be empty");
+      return; // interrupt
+    }
+    //check if password is empty
+    if (password === "") {
+      alert("Password field can not be empty");
+      return; // interrupt
+    }
+
+    // an object with the data to send to a server
+    const data = {
+      username: username,
+      password: password,
+    };
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+
+    // send to server
+    fetch(BACKEND_ROOT_URL + "/auth/login", options)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        console.log("Data sent successfully!");
+      })
+      .catch((error) => {
+        console.error("There was an error!", error);
+      });
+  });
+});
